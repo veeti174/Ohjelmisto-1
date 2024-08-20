44ab228cd5e0195cddb85c6f8356d514d1a8b103
veeti <veeti.isokangas@metropolia.fi>
2024-08-20 11:35:52 +0300

    tehtävä 2.1 kesken

--- Harjoitustehtävä.py
+++ Harjoitustehtävä.py
@@ -4,3 +4,9 @@ print("Hei Veeti Isokangas!")
 
 # Harjotustehtävä 2.1
 
+#Lukee inputtia käyttäjältä
+print("Hei kuka olet?")
+command = "Tee jotain"
+
+#command = input("Anna komento: ")
+#print("Rivinvaihto painettu, komento:" + command)
