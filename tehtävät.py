ec5415e31666cc966b11515a1f72406d1f50fa41
veeti174 <veeti.isokangas@metropolia.fi>
2024-08-22 10:09:25 +0300

    tehtävät t1-2.2 on tehty ja t2.3 on kesken

---
+++ tehtävät.py
@@ -0,0 +1,30 @@
+# Harjoitustehtävä 1.1 ja 1.2
+
+#print("Hei Veeti Isokangas!")
+
+# Harjotustehtävä 2.1
+#Lukee inputtia käyttäjältä
+
+name = input('Kuka olet?: ')
+
+# printtaa inputtia
+
+print("Moi, " + name + "!")
+
+#Harjoitustehtävä 2.2
+
+#Lukee inputtia käyttäjältä
+säde = float(input("Paljonko on ympyrän säde?: "))
+#annetaan pinta
+pintaala = 3.14 * säde ** 2
+#Se laskee ja sitten antaa sen vastauksen
+print("Pinta-ala " + str(säde) + " on: " + str(pintaala))
+
+#Harjoitustehtävä 2.3
+#Lukee inputtia käyttäjältä
+kanta = float(input("Paljonko on ympyrän kanta?: ")
+korkeus = float(input("Paljonko on ympyrän korkeus?: "))
+#annetaan kanta ja korkeus
+pintaala = kanta * korkeus
+#Se laskee ja sitten antaa sen vastauksen
+print("Pinta-ala " + str(säde) + " on: " + str(pintaala))
