f36d76c6d5dc30da8f55793f8ce36c8c46481968
veeti174 <veeti.isokangas@metropolia.fi>
2024-08-29 13:08:02 +0300

    tehtävät moduuli 4 t1, t2, t3, t4, t5 ja t6 tehty

--- Moduuli4/Moduuli4t5.py
+++ Moduuli4/Moduuli4t5.py
@@ -4,18 +4,20 @@ python = 1
 rules = 2
 rounds = 0
 while rounds < 5:
-    while user != python or sala != rules:
-        rounds = rounds + 1
-        user = input("Username input here.: ")
-        sala = input("Password input here.: ")
-        if user == python:
-            print("Username correct. ")
-            sala = input("Password input here.: ")
-            if sala == rules:
-                print("Password correct")
-            if user == python and sala == rules:
-                print("Tervetuloa")
-            else:
+    while user != python or sala != rules or rounds < 5:
+        while rounds < 5:
+            rounds += 1
+            print(f"{rounds}")
+            user = input("Username input here.: ")
+            if user == "python":
+                print("Username correct. ")
+                sala = input("Password input here.: ")
+                if sala == "rules":
+                    print("Tervetuloa")
+                else:
+                    print("Wrong username or password")
+            if sala != user:
+                input("Paasword input here.: ")
                 print("Wrong username or password")
-        else:
-            print("Wrong username or password")
+            else:
+                print("Pääsy evätty")
