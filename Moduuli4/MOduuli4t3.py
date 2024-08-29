76f496b0c3d14aa417ae224a13bf83a9e63fba10
veeti174 <veeti.isokangas@metropolia.fi>
2024-08-29 09:58:21 +0300

    tehtävät moduuli 4 t1 ja t2 tehty t3 ja t6 kesken

--- Moduuli4/MOduuli4t3.py
+++ Moduuli4/MOduuli4t3.py
@@ -1,5 +1,50 @@
 kysy = "1"
 while kysy != "":
     kysy = input("Anna numero ")
-    if kysy == "":
-        print("Moikka")
+    if kysy == "0":
+        num0 = 0
+    elif kysy == "1":
+        num1 = 1
+    elif kysy == "2":
+        num2 = 2
+    elif kysy == "3":
+        num3 = 3
+    elif kysy == "4":
+        num4 = 4
+    elif kysy == "5":
+        num5 = 5
+    elif kysy == "6":
+        num6 = 6
+    elif kysy == "7":
+        num7 = 7
+    elif kysy == "8":
+        num8 = 8
+    elif kysy == "9":
+        num9 = 9
+    elif kysy == "":
+
+        print("min=")
+        print("max=")
+"""
+    print
+    elif kysy == "0":
+        num0 = num0 + 1
+    elif kysy == "1":
+        num1 = num1 + 1
+    elif kysy == "2":
+        num2 = num2 + 1
+    elif kysy == "3":
+        num3 = num3 + 1
+    elif kysy == "4":
+        num4 = num + 1
+    elif kysy == "5":
+        num5 = num5 + 1
+    elif kysy == "6":
+        num6 = num6 + 1
+    elif kysy == "7":
+        num7 = num7 + 1
+    elif kysy == "8":
+        num8 = num8 + 1
+    elif kysy == "9":
+        num9 = num9 + 1
+"""
---
+++ Moduuli4/Moduuli4t6.py
@@ -0,0 +1,27 @@
+# Piin likiarvon laskeminen
+# pii=4n/N, jossa n on yksikköympyrän sisälle osuvat pisteet, N on kaikki pisteet
+# epäyhtälöllä x^2+y^2-1 testataan, onko uksittäinen piste ympyrän sisällä
+# TOinen tapa  importata vain yksittäisiä toimintoja/funktioita
+#from random import randint
+# randint(.1, 1)
+
+import math
+import random
+iterator = 0
+#Todo: Kysy N arvo käyttäjältä
+N = 10000 # pisteiden kokonaismäärä
+n = 0 # ympyrän sisään osuvat pisteet
+while iterator < N:
+    #arvotaan koordinaatiot väliltä -1 ka 1
+    iterator += 1
+    x = random.random() * 2 - 1
+    y = random.uniform(-1, 1)
+    #print(f"Satunnainen piste: {x}, {y}")
+    #print(x**2 +y**2 < 1)
+    if x**2 +y**2 < 1:
+        #print("Osui ympyran sisalle.")
+        n += 1
+print(f"{N} pisteestä {n} osui yksikkö ympyrän sisälle.")
+result = 4 * n / N
+print(f"Piin likiarvo on {result}")
+print(f"Virhe verrattuna math.pi {math.pi:.5f} {result - math.pi:.5f}")
