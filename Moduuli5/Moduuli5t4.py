2290a7b354bf57a3bf6e70ac0c8d7cbde7186504
veeti174 <veeti.isokangas@metropolia.fi>
2024-09-02 14:20:43 +0300

    tehtävät moduuli 5 t1, t2 ja t4 tehty ja t3 kesken

---
+++ Moduuli5/Moduuli5t4.py
@@ -0,0 +1,9 @@
+kaupungit =[]
+for i in range(5):
+    kaupunki= input("Anna kaupunki: ")
+    # lisätään saatu nimi listan loppuun (append)
+    kaupungit.append(kaupunki)
+# järjestetään nimet aakkosjärjestykseen.
+
+print("Nimet aakkosjärjestykset")
+print(kaupungit)
