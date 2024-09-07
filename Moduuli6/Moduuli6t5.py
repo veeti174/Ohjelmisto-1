5189d40deeba5670fef64ce74b734b85cef014cb
veeti174 <veeti.isokangas@metropolia.fi>
2024-09-07 10:50:22 +0300

    tehtävät moduuli 6 t1, t2, t3, t4 ja t6 tehty ja t5 kesken

--- Moduuli6/Moduuli6t5.py
+++ Moduuli6/Moduuli6t5.py
@@ -13,14 +13,47 @@ while arvo != empty:
         break
     arvot.append(arvo)
     arvot.sort()
-
+rounds == rounds -1
 
 
 def pari():
-
     arvotus = arvot
     print(sum(arvotus))
+    if arvot % 2 == 0:
+        print
+
 korva = pari()
 print(korva)
 
 print(arvot)
+
+"""
+empty = (-0)
+arvot = []
+rounds = 0
+arvo = int(input("Anna kokonaisluku yksikerrallaan: "))
+if arvo != empty:
+    rounds +=1
+    arvot.append(arvo)
+    arvot.sort()
+while arvo != empty:
+    arvo = int(input("Anna kokonaisluku yksikerrallaan ja kun olet valmis kirjoita -0: "))
+    rounds += 1
+    if arvo == empty:
+        break
+    arvot.append(arvo)
+    arvot.sort()
+rounds == rounds -1
+
+
+def pari():
+    arvotus = arvot
+    print(sum(arvotus))
+    if arvot % 2 == 0:
+        print
+
+korva = pari()
+print(korva)
+
+print(arvot)
+"""
