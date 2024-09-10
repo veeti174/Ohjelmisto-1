bdfd9ead235a8db09f88db71d3ddf9f2e9d88be4
veeti174 <veeti.isokangas@metropolia.fi>
2024-09-10 11:30:33 +0300

    tehtävät moduuli 7 t1 ja t2 tehty ja t3 kesken

--- Random/Moduuli8esim1.py
+++ Random/Moduuli8esim1.py
@@ -13,27 +13,42 @@ connection = mysql.connector.connect(
          collation='utf8mb4_general_ci'
          )
 #print(connection)
-cursor = connection.cursor()
-sql = "SELECT name, iso_country FROM country"
-sql2 = "SELECT name, iso_country FROM country WHERE name='ä'"
-cursor.execute(sql)
-#result = cursor.fetchone()
-#print(result[0])
-# fetchmany palauttaa n seuraavaa osoittiminen kohdalta
-#result = cursor.fetchmany(3)
-#print(result)
-#fetchall() palauttaa kaikki (loput)
-result = cursor.fetchall()
-# tulosrivien lukumäärä
-print(cursor.rowcount)
-print(result)
-#result = cursor.fetchall()
-#print(*result, sep = "\n")
-#Tulostetaan toisen tulosrivin maakoodi (2. monikon alkio)
-#print(result[1][1])
-# käsitellään koko tulosjoukko (lista) toistorakenteella
-for row in result:
-    print(f"Maan {row[0]} maakoodi {row[1]}.")
-print(f"Maita yhteensä: {cursor.rowcount}")
-if cursor.rowcount == 0:
-    print("Ei yhtään maata.")
+
+def print_countries():
+
+    cursor = connection.cursor()
+    sql = "SELECT name, iso_country FROM country"
+    sql2 = "SELECT name, iso_country FROM country WHERE name='ä'"
+    cursor.execute(sql)
+    #result = cursor.fetchone()
+    #print(result[0])
+    # fetchmany palauttaa n seuraavaa osoittiminen kohdalta
+    #result = cursor.fetchmany(3)
+    #print(result)
+    #fetchall() palauttaa kaikki (loput)
+    result = cursor.fetchall()
+    # tulosrivien lukumäärä
+    print(cursor.rowcount)
+    print(result)
+    #result = cursor.fetchall()
+    #print(*result, sep = "\n")
+    #Tulostetaan toisen tulosrivin maakoodi (2. monikon alkio)
+    #print(result[1][1])
+    # käsitellään koko tulosjoukko (lista) toistorakenteella
+    for row in result:
+        print(f"Maan {row[0]} maakoodi {row[1]}.")
+    print(f"Maita yhteensä: {cursor.rowcount}")
+    if cursor.rowcount == 0:
+        print("Ei yhtään maata.")
+
+print_countries()
+
+def add_country(code, name):
+    sql = f"INSERT INTO country VALUES ('{code}', '{name}', null, null, null)"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    print(cursor.rowcount)
+
+country_name = input("Anna lisättävän Maan nimi: ")
+country_code = input("Anna lisättävän maan koodi: ")
+add_country(country_code, country_name)
