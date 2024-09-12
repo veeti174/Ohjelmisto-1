e15e4cb891a330ee44980788600403866a8a29e9
veeti174 <veeti.isokangas@metropolia.fi>
2024-09-12 09:53:19 +0300

    tehtävät moduuli 7 t1, t2 ja t3 tehty

--- Moduuli7/Moduuli7t3.py
+++ Moduuli7/Moduuli7t3.py
@@ -1,11 +1,50 @@
+"""
 airports = ()
-while true:
-    kysy = input("What do want?\n"
-                 "Input new airport = new\n"
-                 "Search for aiport = aiport\n"
-                 "Lopeta or stop = x")
-    if kysy == x:
+while True:
+    kysy = input("What do want? Search for aiport = 1 Input new airport = 2 Lopeta or stop = x: ")
+    kysy = kysy.lower()
+    if kysy == "x":
         break
-    elif kysy == new:
-
-    elif kysy == airport:
+    elif kysy == "1":
+        ICAO = input("Anna ICAO koodi: ")
+        ICAO = ICAO.upper()
+        for i in airports:
+            if i[0] == ICAO:
+                print(i[1])
+                break
+        else:
+            print("Unknown input")
+    elif kysy == "2":
+        airport = input("Syötä lentokentän nimi: ")
+        ICAO = input("Syötä lentokentän icao koodi: ")
+        airport.capitalize()
+        ICAO = ICAO.upper()
+        i = [ICAO, airport]
+        airports.append(i)
+    else:
+        print("Unknown input")
+"""
+airports = []
+while True:
+    kysy = input("What do want? Search for aiport = 1 Input new airport = 2 Lopeta or stop = x: ")
+    kysy = kysy.lower()
+    if kysy == "x":
+        break
+    elif kysy == "2":
+        airport = input("Syötä lentokentän nimi: ")
+        ICAO = input("Syötä lentokentän icao koodi: ")
+        airport.capitalize()
+        ICAO = ICAO.upper()
+        i = [ICAO, airport]
+        airports.append(i)
+    elif kysy == "1":
+        ICAO = input("Anna ICAO koodi: ")
+        ICAO = ICAO.upper()
+        for i in airports:
+            if i[0] == ICAO:
+                print(i[1])
+                break
+        else:
+            print("Unknown input")
+    else:
+        print("Unknown input")
